6f4ac1783f45478ff76bd12c242c5e3800ea842c
lsonduong <duongls60149@gmail.com>
2022-01-09 15:24:40 +0700

    commit2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld
     public static void main(String args[])
     {
         System.out.println("Hellow World!!!");
-        System.out.println("Jenkin Webhook!!!");
+        System.out.println("Jenkin Webhook1!!!");
     }
 }
