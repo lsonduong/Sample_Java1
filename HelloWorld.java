2d9c24ae1ac2273cc488012b8c62231ef2f127a5
lsonduong <duongls60149@gmail.com>
2022-02-22 16:17:45 +0700

    add new

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld
     public static void main(String args[])
     {
         System.out.println("Hellow World!!!");
-        System.out.println("Jenkin Webhook3!!!");
+        System.out.println("Jenkin Webhook!!!");
         System.out.println("Jenkin Params!!!");
     }
 }
